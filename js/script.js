8bb340a8adcab14504eef38c8e0f1956339fdc40
enrique <santosenrique869@gmail.com>
2022-04-17 07:21:11 -0400

    alteração no paddind do slider cards

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 let containerSliderWidth = document.querySelector('.slider-width'),
-    quantSliders = document.querySelectorAll('.slider'),
-    sliderBottunBox = document.querySelector('.botoes-slider'),
-    boxButtunCard = document.querySelector('.button-slider-team'),
-    containerSliderWidthCard = document.querySelector('.slider-width-team'),
-    menuNav = document.querySelector('.menu nav'),
-    menuLi = document.querySelectorAll('.menu nav ul li'),
-    menuLinkActive = document.querySelectorAll('.menu nav ul li a'),
-    gallery = document.querySelector('.grid-gallery--pics');
+  quantSliders = document.querySelectorAll('.slider'),
+  sliderBottunBox = document.querySelector('.botoes-slider'),
+  boxButtunCard = document.querySelector('.button-slider-team'),
+  containerSliderWidthCard = document.querySelector('.slider-width-team'),
+  menuNav = document.querySelector('.menu nav'),
+  menuLi = document.querySelectorAll('.menu nav ul li'),
+  menuLinkActive = document.querySelectorAll('.menu nav ul li a'),
+  gallery = document.querySelector('.grid-gallery--pics');
 
 // menu active
 menuLinkActive.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        document.querySelector('.menu nav ul li.active').classList.remove('active');
-        menuLi[index].classList.add('active');
-    });
+  item.addEventListener('click', () => {
+    document.querySelector('.menu nav ul li.active').classList.remove('active');
+    menuLi[index].classList.add('active');
+  });
 });
 
 // widht dinamic container sliders and buttons dinamic insert
 function insertWidthContent(conatiner, lenghtSliders, unity) {
-    conatiner.style.width = `${lenghtSliders * 100}${unity}`;
+  conatiner.style.width = `${lenghtSliders * 100}${unity}`;
 }
 
 function InsertButtonSpans(lenghtItemsContainer, parentElement) {
-    for (let i = 0; i < lenghtItemsContainer; i++) {
-        let span = document.createElement('span');
-        span.classList.add('default-button-slider');
-        span.setAttribute(`data-key`, `${i}`);
-        if (i == 0) {
-            span.classList.add('active');
-        }
-        parentElement.append(span);
+  for (let i = 0; i < lenghtItemsContainer; i++) {
+    let span = document.createElement('span');
+    span.classList.add('default-button-slider');
+    span.setAttribute(`data-key`, `${i}`);
+    if (i == 0) {
+      span.classList.add('active');
     }
+    parentElement.append(span);
+  }
 }
 
 insertWidthContent(containerSliderWidth, quantSliders.length, 'vw');
@@ -38,29 +38,29 @@ InsertButtonSpans(quantSliders.length, sliderBottunBox);
 
 // next and prev slider function
 document.querySelectorAll('.botoes-slider span').forEach(item => {
-    item.addEventListener('click', e => {
-        document.querySelector('.botoes-slider span.active').classList.remove('active');
-        let marginVal = `-${item.getAttribute('data-key') * 100}vw`;
-        containerSliderWidth.style.marginLeft = marginVal;
-        e.target.classList.add('active');
-        currentIndex = item.getAttribute('data-key');
-        clearInterval(sliderAuto);
-        sliderAuto = setInterval(nextSlider, 5000);
-    });
+  item.addEventListener('click', e => {
+    document.querySelector('.botoes-slider span.active').classList.remove('active');
+    let marginVal = `-${item.getAttribute('data-key') * 100}vw`;
+    containerSliderWidth.style.marginLeft = marginVal;
+    e.target.classList.add('active');
+    currentIndex = item.getAttribute('data-key');
+    clearInterval(sliderAuto);
+    sliderAuto = setInterval(nextSlider, 5000);
+  });
 });
 
 // slider automatic
 let currentIndex = 0;
 function nextSlider() {
-    document.querySelector('.botoes-slider span.active').classList.remove('active');
-    if (currentIndex == quantSliders.length - 1) {
-        currentIndex = 0;
-        containerSliderWidth.style.marginLeft = `${currentIndex}vw`;
-    } else {
-        currentIndex++;
-        containerSliderWidth.style.marginLeft = `-${currentIndex * 100}vw`;
-    }
-    document.querySelectorAll('.botoes-slider span')[currentIndex].classList.add('active');
+  document.querySelector('.botoes-slider span.active').classList.remove('active');
+  if (currentIndex == quantSliders.length - 1) {
+    currentIndex = 0;
+    containerSliderWidth.style.marginLeft = `${currentIndex}vw`;
+  } else {
+    currentIndex++;
+    containerSliderWidth.style.marginLeft = `-${currentIndex * 100}vw`;
+  }
+  document.querySelectorAll('.botoes-slider span')[currentIndex].classList.add('active');
 }
 // Run slider auto
 let sliderAuto = setInterval(nextSlider, 5000);
@@ -68,173 +68,173 @@ let sliderAuto = setInterval(nextSlider, 5000);
 // Menu active
 let btnMobile = document.querySelector('.btn-mobile');
 btnMobile.addEventListener('click', () => {
-    btnMobile.classList.toggle('active');
-    menuNav.classList.toggle('active');
+  btnMobile.classList.toggle('active');
+  menuNav.classList.toggle('active');
 });
 
 // maker gallery
 let nodeCopy = document.querySelector('.photo');
 photosGallery[0].all.map((item, indexOf) => {
-    let photoItem = document.querySelector('.photo').cloneNode(true);
+  let photoItem = document.querySelector('.photo').cloneNode(true);
 
-    photoItem.style.backgroundImage = `url('${item.img}')`;
-    photoItem.querySelector('h5').innerHTML = 'about';
-    photoItem.querySelector('p').innerHTML = item.descri;
+  photoItem.style.backgroundImage = `url('${item.img}')`;
+  photoItem.querySelector('h5').innerHTML = 'about';
+  photoItem.querySelector('p').innerHTML = item.descri;
 
-    gallery.append(photoItem);
+  gallery.append(photoItem);
 });
 
 let menuChangePhotos = document.querySelectorAll('.change-options ul li');
 menuChangePhotos.forEach(el => {
-    el.addEventListener('click', event => {
-        document.querySelector('.change-options li.active').classList.remove('active');
-        el.classList.add('active');
-        let key = el.getAttribute('data-key');
-        gallery.innerHTML = '';
+  el.addEventListener('click', event => {
+    document.querySelector('.change-options li.active').classList.remove('active');
+    el.classList.add('active');
+    let key = el.getAttribute('data-key');
+    gallery.innerHTML = '';
 
-        switch (key) {
-            case 'all':
-                photosGallery[0].all.map((item, index) => {
-                    let photoItem = document.querySelector('.photo').cloneNode(true);
+    switch (key) {
+      case 'all':
+        photosGallery[0].all.map((item, index) => {
+          let photoItem = document.querySelector('.photo').cloneNode(true);
 
-                    photoItem.style.backgroundImage = `url('${item.img}')`;
-                    photoItem.querySelector('h5').innerHTML = 'about';
-                    photoItem.querySelector('p').innerHTML = item.descri;
+          photoItem.style.backgroundImage = `url('${item.img}')`;
+          photoItem.querySelector('h5').innerHTML = 'about';
+          photoItem.querySelector('p').innerHTML = item.descri;
 
-                    gallery.append(photoItem);
-                });
-                break;
-            case 'photos':
-                photosGallery[1].models.map((item, index) => {
-                    let photoItem = document.querySelector('.photo').cloneNode(true);
+          gallery.append(photoItem);
+        });
+        break;
+      case 'photos':
+        photosGallery[1].models.map((item, index) => {
+          let photoItem = document.querySelector('.photo').cloneNode(true);
 
-                    photoItem.style.backgroundImage = `url('${item.img}')`;
-                    photoItem.querySelector('h5').innerHTML = 'about';
-                    photoItem.querySelector('p').innerHTML = item.descri;
+          photoItem.style.backgroundImage = `url('${item.img}')`;
+          photoItem.querySelector('h5').innerHTML = 'about';
+          photoItem.querySelector('p').innerHTML = item.descri;
 
-                    gallery.append(photoItem);
-                });
-                break;
-            case 'branding':
-                photosGallery[2].branding.map((item, index) => {
-                    let photoItem = document.querySelector('.photo').cloneNode(true);
+          gallery.append(photoItem);
+        });
+        break;
+      case 'branding':
+        photosGallery[2].branding.map((item, index) => {
+          let photoItem = document.querySelector('.photo').cloneNode(true);
 
-                    photoItem.style.backgroundImage = `url('${item.img}')`;
-                    photoItem.querySelector('h5').innerHTML = 'about';
-                    photoItem.querySelector('p').innerHTML = item.descri;
+          photoItem.style.backgroundImage = `url('${item.img}')`;
+          photoItem.querySelector('h5').innerHTML = 'about';
+          photoItem.querySelector('p').innerHTML = item.descri;
 
-                    gallery.append(photoItem);
-                });
-                break;
-            case 'adverts':
-                photosGallery[3].adverts.map((item, index) => {
-                    let photoItem = document.querySelector('.photo').cloneNode(true);
+          gallery.append(photoItem);
+        });
+        break;
+      case 'adverts':
+        photosGallery[3].adverts.map((item, index) => {
+          let photoItem = document.querySelector('.photo').cloneNode(true);
 
-                    photoItem.style.backgroundImage = `url('${item.img}')`;
-                    photoItem.querySelector('h5').innerHTML = 'about';
-                    photoItem.querySelector('p').innerHTML = item.descri;
+          photoItem.style.backgroundImage = `url('${item.img}')`;
+          photoItem.querySelector('h5').innerHTML = 'about';
+          photoItem.querySelector('p').innerHTML = item.descri;
 
-                    gallery.append(photoItem);
-                });
-                break;
-            case 'development':
-                photosGallery[4].development.map((item, index) => {
-                    let photoItem = document.querySelector('.photo').cloneNode(true);
+          gallery.append(photoItem);
+        });
+        break;
+      case 'development':
+        photosGallery[4].development.map((item, index) => {
+          let photoItem = document.querySelector('.photo').cloneNode(true);
 
-                    photoItem.style.backgroundImage = `url('${item.img}')`;
-                    photoItem.querySelector('h5').innerHTML = 'about';
-                    photoItem.querySelector('p').innerHTML = item.descri;
+          photoItem.style.backgroundImage = `url('${item.img}')`;
+          photoItem.querySelector('h5').innerHTML = 'about';
+          photoItem.querySelector('p').innerHTML = item.descri;
 
-                    gallery.append(photoItem);
-                });
-                break;
-            default:
-                break;
-        }
-    });
+          gallery.append(photoItem);
+        });
+        break;
+      default:
+        break;
+    }
+  });
 });
 
 // More projects
 let buttonMoreProjects = document.querySelector('.more-projects button');
 buttonMoreProjects.addEventListener('click', () => {
-    buttonMoreProjects.classList.toggle('active');
-    if (buttonMoreProjects.classList.contains('active') == true) {
-        buttonMoreProjects.textContent = 'less projects';
-    } else {
-        buttonMoreProjects.textContent = 'more projects';
-        gallery.scrollTop = 0;
-    }
-    gallery.classList.toggle('active');
+  buttonMoreProjects.classList.toggle('active');
+  if (buttonMoreProjects.classList.contains('active') == true) {
+    buttonMoreProjects.textContent = 'less projects';
+  } else {
+    buttonMoreProjects.textContent = 'more projects';
+    gallery.scrollTop = 0;
+  }
+  gallery.classList.toggle('active');
 });
 
 // maker card team
 
 cardInfo.map((item, index) => {
-    let cardModel = document.querySelector('.content-cards').cloneNode(true);
-    cardModel.innerHTML = '';
-    cardModel.setAttribute('data-key', `${index}`);
-    if (index == 0) {
-        item.cards1.forEach(item => {
-            let card = document.querySelector('.cards-slider').cloneNode(true);
-            card.querySelector('img').src = item.img;
-            card.querySelector('h5').innerHTML = item.name;
-            card.querySelector('span').innerHTML = item.descri;
-            cardModel.append(card);
-            document.querySelector('.slider-width-team').appendChild(cardModel);
-        });
-    }
-    if (index == 1) {
-        item.cards2.forEach(item => {
-            let card = document.querySelector('.cards-slider').cloneNode(true);
-            card.querySelector('img').src = item.img;
-            card.querySelector('h5').innerHTML = item.name;
-            card.querySelector('span').innerHTML = item.descri;
-            cardModel.append(card);
-            document.querySelector('.slider-width-team').appendChild(cardModel);
-        });
-    }
-    if (index == 2) {
-        item.cards3.forEach(item => {
-            let card = document.querySelector('.cards-slider').cloneNode(true);
-            card.querySelector('img').src = item.img;
-            card.querySelector('h5').innerHTML = item.name;
-            card.querySelector('span').innerHTML = item.descri;
-            cardModel.append(card);
-            document.querySelector('.slider-width-team').appendChild(cardModel);
-        });
-    }
+  let cardModel = document.querySelector('.content-cards').cloneNode(true);
+  cardModel.innerHTML = '';
+  cardModel.setAttribute('data-key', `${index}`);
+  if (index == 0) {
+    item.cards1.forEach(item => {
+      let card = document.querySelector('.cards-slider').cloneNode(true);
+      card.querySelector('img').src = item.img;
+      card.querySelector('h5').innerHTML = item.name;
+      card.querySelector('span').innerHTML = item.descri;
+      cardModel.append(card);
+      document.querySelector('.slider-width-team').appendChild(cardModel);
+    });
+  }
+  if (index == 1) {
+    item.cards2.forEach(item => {
+      let card = document.querySelector('.cards-slider').cloneNode(true);
+      card.querySelector('img').src = item.img;
+      card.querySelector('h5').innerHTML = item.name;
+      card.querySelector('span').innerHTML = item.descri;
+      cardModel.append(card);
+      document.querySelector('.slider-width-team').appendChild(cardModel);
+    });
+  }
+  if (index == 2) {
+    item.cards3.forEach(item => {
+      let card = document.querySelector('.cards-slider').cloneNode(true);
+      card.querySelector('img').src = item.img;
+      card.querySelector('h5').innerHTML = item.name;
+      card.querySelector('span').innerHTML = item.descri;
+      cardModel.append(card);
+      document.querySelector('.slider-width-team').appendChild(cardModel);
+    });
+  }
 });
 
 // dinamic quant span buttons card slider
 let lenghtCards = document.querySelectorAll('.content-cards'),
-    containerCardWidth = document.querySelector('.slider-width-team');
+  containerCardWidth = document.querySelector('.slider-width-team');
 
 insertWidthContent(containerCardWidth, `${lenghtCards.length - 1}`, '%');
 InsertButtonSpans(lenghtCards.length - 1, boxButtunCard);
 
 let indexCard = 0;
 function nextSliderCard() {
-    document.querySelector('.button-slider-team span.active').classList.remove('active');
-    if (indexCard == lenghtCards.length - 2) {
-        indexCard = 0;
-        containerCardWidth.style.marginLeft = `${indexCard}`;
-    } else {
-        indexCard++;
-        containerCardWidth.style.marginLeft = `-${indexCard * 100}%`;
-    }
-    document.querySelectorAll('.button-slider-team span')[indexCard].classList.add('active');
+  document.querySelector('.button-slider-team span.active').classList.remove('active');
+  if (indexCard == lenghtCards.length - 2) {
+    indexCard = 0;
+    containerCardWidth.style.marginLeft = `${indexCard}`;
+  } else {
+    indexCard++;
+    containerCardWidth.style.marginLeft = `-${indexCard * 100}%`;
+  }
+  document.querySelectorAll('.button-slider-team span')[indexCard].classList.add('active');
 }
 let cardAuto = setInterval(nextSliderCard, 7000);
 
 // manul slider card
 document.querySelectorAll('.button-slider-team span').forEach(item => {
-    item.addEventListener('click', e => {
-        document.querySelector('.button-slider-team span.active').classList.remove('active');
-        let marginValCard = `-${item.getAttribute('data-key') * 100}%`;
-        containerCardWidth.style.marginLeft = marginValCard;
-        e.target.classList.add('active');
-        indexCard = item.getAttribute('data-key');
-        clearInterval(cardAuto);
-        cardAuto = setInterval(nextSliderCard, 7000);
-    });
+  item.addEventListener('click', e => {
+    document.querySelector('.button-slider-team span.active').classList.remove('active');
+    let marginValCard = `-${item.getAttribute('data-key') * 100}%`;
+    containerCardWidth.style.marginLeft = marginValCard;
+    e.target.classList.add('active');
+    indexCard = item.getAttribute('data-key');
+    clearInterval(cardAuto);
+    cardAuto = setInterval(nextSliderCard, 7000);
+  });
 });
